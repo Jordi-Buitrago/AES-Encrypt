79ecb0d283b086b05063f53ba4214c855fb93eba
Jordi Buitrago <jordibuimar@gmail.com>
2022-10-31 13:14:25 +0100

    first commit

---
+++ AES/Program.cs
@@ -0,0 +1,51 @@
+﻿int menu = 0;
+while (menu != 3)
+{
+
+    {
+        Console.WriteLine("[1] Encriptar");
+        Console.WriteLine("[2] Desencriptar");
+        Console.WriteLine("[3] Sortir");
+        Console.Write("Introdueix l'opcio que vols realitzar: ");
+        menu = int.Parse(Console.ReadLine());
+        switch (menu)
+        {
+            case 1:
+                Console.Write("Introdueix el nom del arxiu a encriptar: ");
+                String nArxiu = Console.ReadLine();
+                nArxiu = File.ReadAllText(nArxiu);
+                Console.WriteLine("Introdueix la frase de pas: ");
+                String pasPhrase = Console.ReadLine();
+                String encriptat = AES.Xifrat.Encrypt(nArxiu, pasPhrase, "12345678");
+                Console.WriteLine("El text desencriptat es aquest: " + encriptat);
+                File.WriteAllText("encriptat.txt", encriptat);
+                break;
+
+            case 2:
+                Console.Write("Introdueix el nom del arxiu a desencriptar: ");
+                String nArxiuD = Console.ReadLine();
+                nArxiu = File.ReadAllText(nArxiuD);
+                if (nArxiuD != "encriptat.txt")
+                {
+                    Console.WriteLine("No existeix aquest arxiu, prova (encriptat.txt)");
+                }
+                else
+                {
+                    Console.WriteLine("Introdueix la frase de pas: ");
+                    String pasPhraseD = Console.ReadLine();
+                    String desencriptat = AES.Xifrat.Decrypt(nArxiu, pasPhraseD, "12345678");
+                    Console.WriteLine("El text desencriptat es aquest: " + desencriptat);
+                    File.WriteAllText("desencriptat.txt", desencriptat);
+                }
+                break;
+
+
+            case 3:
+                break;
+
+        }
+
+
+    }
+
+}
---
+++ AES/Xifrat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AES
+{
+    internal class Xifrat
+    {
+        public static string Encrypt(string message, string pass, string salt)
+        {
+            //Aquesta funcio fa el cifrat i descifrat
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            //Aqui agafem sequencies de bytes del salt i la longitud que volem
+            DeriveBytes rgb = new Rfc2898DeriveBytes(pass, Encoding.Unicode.GetBytes(salt), 9);
+            //Determinem el congunt de tamany de la clau 
+            byte[] key = rgb.GetBytes(aes.KeySize >> 3);
+            //Determinem el congunt de tamany del cifrat de bloc
+            byte[] iv = rgb.GetBytes(aes.BlockSize >> 3);
+            //Especifiquem el mode de cifrat de bloc
+            aes.Mode = CipherMode.CBC;
+            aes.Key = key;
+            aes.IV = iv;
+            //Definim les operacions bàsiques i 
+            ICryptoTransform encryptor = aes.CreateEncryptor();
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            byte[] dataencrypt = encryptor.TransformFinalBlock(data, 0, data.Length);
+            //retorna un array a String
+            return Convert.ToBase64String(dataencrypt);
+        }
+        public static string Decrypt(string message, string pass, string salt)  
+        {
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            DeriveBytes rgb = new Rfc2898DeriveBytes(pass, Encoding.Unicode.GetBytes(salt), 9);
+            byte[] key = rgb.GetBytes(aes.KeySize >> 3);
+            byte[] iv = rgb.GetBytes(aes.BlockSize >> 3);
+            aes.Mode = CipherMode.CBC;
+            aes.Key = key;
+            aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor();
+            byte[] data = Convert.FromBase64String(message);
+            byte[] datadecrypt = decryptor.TransformFinalBlock(data, 0, data.Length);
+            return Encoding.Unicode.GetString(datadecrypt);
+        }
+
+    }
+}
